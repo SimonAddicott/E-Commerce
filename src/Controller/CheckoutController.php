b9fc11ce4b3af643987718912c7ee12d5b494fd4
Simon Addicott <simonaddicott@simons-mbp.home>
2021-09-24 00:40:33 +0100

    Added in checkouts, needs to be finished with order editing then move onto paypal integration

---
+++ src/Controller/CheckoutController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
+use App\Entity\Product;
+
+class CheckoutController extends AbstractController
+{
+    private $requestStack;
+    
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+    
+    
+    /**
+     * @Route("/checkout", name="checkout")
+     */
+    public function index(): Response
+    {
+        $checkout = [];
+        $session = $this->requestStack->getCurrentRequest()->getSession();
+        
+        $productRepository = $this->getDoctrine()->getManager()->getRepository(Product::class);
+        
+        if($session->has('basket')) { 
+            $basket = $session->get('basket');
+            $basketTotal = 0;
+            foreach ($basket as $productId => $quantity)
+            {
+                if(!is_numeric($productId))
+                    continue;
+                
+                $productData = $productRepository->find((int)$productId);
+                
+                if(!$productData)
+                    return $this->render('error_page.html.twig', ['error' => 'product id = ' . $productId]);
+                
+                $itemIotal = $productData->getPrice() * (int)$quantity;
+                $basketTotal += $itemIotal;
+                
+                $checkoutItem = [
+                    'product' => $productData,
+                    'quantity' => $quantity,
+                    'item_total' => $itemIotal
+                ];
+                $checkout['products'][] = $checkoutItem;
+            }
+            $checkout['basket_total'] = $basketTotal;
+            
+            // get product data for basket contents
+        }
+        return $this->render('checkout/index.html.twig', [
+            'controller_name' => 'CheckoutController',
+            'basket' => $checkout
+        ]);
+    }
+}
