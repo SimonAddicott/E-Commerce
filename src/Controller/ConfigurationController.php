e00534e56044895c6f4bae17bc4948af8b636d12
Simon Addicott <simonaddicott@simons-mbp.home>
2021-11-04 22:01:57 +0000

    Add guest purchasing

--- src/Controller/ConfigurationController.php
+++ src/Controller/ConfigurationController.php
@@ -5,16 +5,49 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Configuration;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Doctrine\ORM\EntityNotFoundException;
 
 class ConfigurationController extends AbstractController
 {
-    /**
-     * @Route("/configuration", name="configuration")
-     */
-    public function index(): Response
+    private $requestStack;
+    
+    public function __construct(RequestStack $requestStack)
     {
-        return $this->render('configuration/index.html.twig', [
-            'controller_name' => 'ConfigurationController',
-        ]);
+        $this->requestStack = $requestStack;
+    }
+    
+    private function loadConfig()
+    {
+        $session = $this->requestStack->getCurrentRequest()->getSession();
+        $repository = $this->getDoctrine()->getManager()->getRepository(Configuration::class);
+        $config = $repository->findAll();
+        if(!$config)
+        {
+            $configSession = [];
+            foreach($config as $configItem)
+            {
+                $configSession[$configItem->getName()] = $configItem->getValue();
+            }
+            $session->set('config', $configSession);
+        }
+    }
+    
+    public function requestConfigByName(string $name)
+    {
+        $session = $this->requestStack->getCurrentRequest()->getSession();
+        
+        if (!$session->get('config'))
+        {
+            $this->loadConfig();
+        }
+        
+        if(isset($session[$name]))
+        {
+            return $session[$name];
+        }
+        
+        throw new EntityNotFoundException("No configuration found for '$name'");
     }
 }
